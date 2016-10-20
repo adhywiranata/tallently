989bdf07a5e6450844cc0265dc445eeefbc5b5ce
Adhy <adhywiranatap@gmail.com>
2016-10-20 23:31:29 +0700

    updated semantic ui react views

---
+++ src/components/Button/BigButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const BigButton = (props) => (
+  <Button>
+    hey there!
+  </Button>
+)
+
+export default BigButton
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,15 @@
 import React, { Component, PropTypes }  from 'react'
 import css from './home.css'
+import BigButton from '../../components/Button/BigButton'
 
 class Home extends Component {
   render () {
     return (
-      <div className={css.hi}>
-        hello C4TK!
+      <div className={css.home}>
+        <h1 className={css.hi}>
+          hello C4TK!
+        </h1>
+        <BigButton/>
       </div>
     )
   }
