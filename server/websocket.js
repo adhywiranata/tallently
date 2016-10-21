4078ce539649ca298881be7f364dd03dc79203bb
Adhy <adhywiranatap@gmail.com>
2016-10-21 21:17:14 +0700

    updated packages

---
+++ server/websocket.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+let app     = express()
+const path  = require('path')
+
+app.use(express.static('static'))
+app.use('/public', express.static(path.join(__dirname, '../public')))
+app.use('/app', express.static(path.join(__dirname, '../src/index.html')))
+/*
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../src/', 'dummy.html'))
+})
+*/
+
+app.listen(5000, function () {
+  console.log('Tallently listening on port 5000!');
+});
+// express nya disini ya hen
